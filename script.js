2fd514ac1aeacce5eb5fa4b3f7cbb136f3e487ed
Rebecca D. <rdolmon08@gmail.com>
2024-07-26 13:53:55 -0500

    adding event listeners

--- script.js
+++ script.js
@@ -60,8 +60,11 @@ function calculateCalories(e) {
   <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
   <hr>
   <p>${budgetCalories} Calories Budgeted</p>
-
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
   `;
+
+  output.classList.remove('hide');
 }
 
 function getCaloriesFromInputs(list) {
@@ -81,4 +84,9 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {
+
+}
+
 addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
